fde345327bdc74d13bb15f0ac7ce77523e3c5fb3
Ashish <59895653+ashdiyali@users.noreply.github.com>
2020-11-19 14:31:49 -0600

    update

--- recipes.sql
+++ recipes.sql
@@ -1,14 +1,22 @@
+
+
+
+
+
+
+
+-- creating 3 table  ( Recipe, Ingrediemt and measure) with Null values and how many character each one will get --
 create table Recipe (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(25), description VARCHAR(50), instructions VARCHAR(500)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 create table Ingredient (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50)) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
 create table Measure (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(30)) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
-
+-- Table created with authentication to reterive the right data when the query is made
 create table RecipeIngredient (recipe_id INT NOT NULL, ingredient_id INT NOT NULL, measure_id INT, amount INT, 
 	CONSTRAINT fk_recipe FOREIGN KEY(recipe_id) REFERENCES Recipe(id), 
 	CONSTRAINT fk_ingredient FOREIGN KEY(ingredient_id) REFERENCES Ingredient(id), 
 	CONSTRAINT fk_measure FOREIGN KEY(measure_id) REFERENCES Measure(id)) 
 	ENGINE=InnoDB DEFAULT CHARSET=utf8; 
 
-
+--Data created 
 INSERT INTO measure (name) VALUES('CUP'), ('TEASPOON'), ('TABLESPOON'), ('WHOLE');
 
 INSERT INTO Ingredient (name) VALUES('egg'), ('salt'), ('sugar'), ('chocolate'), ('vanilla extract'), ('flour');
@@ -16,6 +24,14 @@ INSERT INTO Ingredient (name) VALUES('egg'), ('salt'), ('sugar'), ('chocolate'),
 INSERT INTO Recipe (name, description, instructions) VALUES('Boiled Egg', 'A single boiled egg', 'Add egg to cold water. Bring water to boil. Cook.');
 
 INSERT INTO Recipe (name, description, instructions) VALUES('Chocolate Cake', 'Yummy cake', 'Add eggs, flour, chocolate to pan. Bake at 350 for 1 hour');
+
+INSERT INTO Recipe (name, description, instructions) VALUES ('Tea','mix tea leaves to hot water and enjoy')
+INSERT INTO Recipe (name, description, instructions) VALUES ('Omlet', '')
+INSERT INTO Recipe (name, description, instructions) VALUES ('')
+INSERT INTO Recipe (name, description, instructions) VALUES ('')
+INSERT INTO Recipe (name, description, instructions) VALUES ('')
+
+
 
 INSERT INTO RecipeIngredient (recipe_id, ingredient_id, measure_id, amount) VALUES (1, 1, NULL, 1);
 
@@ -25,4 +41,13 @@ INSERT INTO RecipeIngredient (recipe_id, ingredient_id, measure_id, amount)  VAL
 
 INSERT INTO RecipeIngredient (recipe_id, ingredient_id, measure_id, amount)  VALUES (2, 3, 1, 2);
 
+INSERT INTO RecipeIngredient (recipe_id, ingredient_id, measure_id, amount)  VALUES (2, 4, 1, 1);
+
+
+
+
+INSERT INTO RecipeIngredient (recipe_id, ingredient_id, measure_id, amount)  VALUES (1, 3, NULL, NULL);
+INSERT INTO RecipeIngredient (recipe_id, ingredient_id, measure_id, amount)  VALUES (2, 4, 1, 1);
+INSERT INTO RecipeIngredient (recipe_id, ingredient_id, measure_id, amount)  VALUES (2, 4, 1, 1);
+INSERT INTO RecipeIngredient (recipe_id, ingredient_id, measure_id, amount)  VALUES (2, 4, 1, 1);
 INSERT INTO RecipeIngredient (recipe_id, ingredient_id, measure_id, amount)  VALUES (2, 4, 1, 1);
